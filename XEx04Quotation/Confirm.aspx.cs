ea4b0495a2d09328b417c596ab4afcf94f6ca90f
davindergill <44737744+davindergill@users.noreply.github.com>
2018-11-03 21:32:41 -0400

    Initial commit

---
+++ XEx04Quotation/Confirm.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace XEx04Quotation
+{
+    public partial class Confirm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            if (txtEmail == null || txtName == null)
+            {
+                Label1.Text = "Please Provide the email and name";
+            }
+            else
+
+                Label1.Text = "Thank You for sending the quotation !   Click on Return to go back to Home Page";
+        }
+    }
+}
